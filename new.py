8c3eeb26ac63634d5ee64f5bc38e8fd771f09c5a
Ned Batchelder <ned@nedbatchelder.com>
2023-12-03 08:54:47 -0500

    starter template

---
+++ new.py
@@ -0,0 +1,29 @@
+# https://adventofcode.com/2023/day/X
+
+import re
+from dataclasses import dataclass
+
+from helpers import *
+
+TEST_INPUT = string_lines(
+    """\
+"""
+)
+
+
+# def part1(lines):
+#     ...
+# 
+# 
+# if __name__ == "__main__":
+#     answer = part1(file_lines("dayX_input.txt"))
+#     print(f"Part 1: {answer = }")
+
+
+# def part2(lines):
+#     ...
+# 
+# 
+# if __name__ == "__main__":
+#     answer = part2(file_lines("dayX_input.txt"))
+#     print(f"Part 2: {answer = }")
