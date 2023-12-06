21b06827ea2afd5cad3e737415e8d5080067e901
Ned Batchelder <ned@nedbatchelder.com>
2023-12-06 06:14:59 -0500

    day 6, part 2

--- day06.py
+++ day06.py
@@ -20,8 +20,10 @@ INPUT = [
 def distance(race_time, button_time):
     return (race_time - button_time) * button_time
 
+
 def distances(race_time):
     return [distance(race_time, btime) for btime in range(0, race_time + 1)]
+
 
 def test_distances():
     assert distances(7) == [0, 6, 10, 12, 12, 10, 6, 0]
@@ -30,12 +32,17 @@ def test_distances():
 def ways_to_beat_record(race_time, record):
     return sum(dist > record for dist in distances(race_time))
 
+
 def test_ways_to_beat_record():
-    assert [ways_to_beat_record(race_time, record) for race_time, record in TEST_INPUT] == [4, 8, 9]
+    assert [
+        ways_to_beat_record(race_time, record) for race_time, record in TEST_INPUT
+    ] == [4, 8, 9]
 
 
 def part1(times_records):
-    return math.prod(ways_to_beat_record(race_time, record) for race_time, record in times_records)
+    return math.prod(
+        ways_to_beat_record(race_time, record) for race_time, record in times_records
+    )
 
 
 def test_part1():
@@ -47,15 +54,15 @@ if __name__ == "__main__":
     print(f"Part 1: {answer = }")
 
 
-# def part2(lines):
-#     ...
-#
-#
-# def test_part2():
-#     assert part2(TEST_INPUT) == 123456
-#
-#
-#
-# if __name__ == "__main__":
-#     answer = part2(file_lines("day06_input.txt"))
-#     print(f"Part 2: {answer = }")
+TEST_INPUT2 = [(71530, 940200)]
+
+
+def test_part2():
+    assert part1(TEST_INPUT2) == 71503
+
+
+INPUT2 = [(48876981, 255128811171623)]
+
+if __name__ == "__main__":
+    answer = part1(INPUT2)
+    print(f"Part 2: {answer = }")
