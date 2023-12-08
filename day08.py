d308f608a6befc49dce56d9cdfcc1433a1395932
Ned Batchelder <ned@nedbatchelder.com>
2023-12-08 05:53:41 -0500

    day 8, part 2

--- day08.py
+++ day08.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2023/day/08
 
 import itertools
+import math
 import re
 from dataclasses import dataclass
 
@@ -46,15 +47,47 @@ if __name__ == "__main__":
     print(f"Part 1: {answer = }")
 
 
-# def part2(lines):
-#     ...
-#
-#
-# def test_part2():
-#     assert part2(TEST_INPUT) == 123456
-#
-#
-#
-# if __name__ == "__main__":
-#     answer = part2(file_lines("day08_input.txt"))
-#     print(f"Part 2: {answer = }")
+TEST_INPUT2 = string_lines("""\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+""")
+
+
+# This part2 is straightforward, but takes too long:
+
+def part2(lines):
+    left_right, nodes = parse_input(lines)
+    my_nodes = [node for node in nodes if node.endswith("A")]
+    for steps, lr in enumerate(itertools.cycle(left_right), start=1):
+        my_nodes = [nodes[node][lr == "R"] for node in my_nodes]
+        if all(node.endswith("Z") for node in my_nodes):
+            break
+    return steps
+
+def part2(lines):
+    left_right, nodes = parse_input(lines)
+    my_nodes = [node for node in nodes if node.endswith("A")]
+    stepss = []
+    for node in my_nodes:
+        for steps, lr in enumerate(itertools.cycle(left_right), start=1):
+            node = nodes[node][lr == "R"]
+            if node.endswith("Z"):
+                break
+        stepss.append(steps)
+    return math.lcm(*stepss)
+
+def test_part2():
+    assert part2(TEST_INPUT2) == 6
+
+
+if __name__ == "__main__":
+    answer = part2(file_lines("day08_input.txt"))
+    print(f"Part 2: {answer = }")
