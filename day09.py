bc0cadf4d9f3fb0a70ebbbcbc5f3fbfa10ab0126
Ned Batchelder <ned@nedbatchelder.com>
2023-12-09 07:12:00 -0500

    day 9, part 2

--- day09.py
+++ day09.py
@@ -15,15 +15,15 @@ def differences(nums):
     for a, b in zip(nums, nums[1:]):
         yield b - a
 
-def next_value(nums):
+def next_value(nums, factor=1):
     sequences = [list(nums)]
     while set(sequences[-1]) != {0}:
         sequences.append(list(differences(sequences[-1])))
     sequences.reverse()
     sequences[0].append(0)
     for seq1, seq2 in zip(sequences, sequences[1:]):
-        seq2.append(seq1[-1] + seq2[-1])
-    return sequences[-1][-1]
+        seq2.append(seq1[-1] + factor * seq2[-1])
+    return factor * sequences[-1][-1]
 
 def parse_sequences(lines):
     return [list(map(int, line.split())) for line in lines]
@@ -46,15 +46,17 @@ if __name__ == "__main__":
     print(f"Part 1: {answer = }")
 
 
-# def part2(lines):
-#     ...
-#
-#
-# def test_part2():
-#     assert part2(TEST_INPUT) == 123456
-#
-#
-#
-# if __name__ == "__main__":
-#     answer = part2(file_lines("day09_input.txt"))
-#     print(f"Part 2: {answer = }")
+def part2(lines):
+    seqs = parse_sequences(lines)
+    seqs = [list(reversed(seq)) for seq in seqs]
+    return sum(next_value(seq, -1) for seq in seqs)
+
+
+def test_part2():
+    assert part2(TEST_INPUT) == 2
+
+
+
+if __name__ == "__main__":
+    answer = part2(file_lines("day09_input.txt"))
+    print(f"Part 2: {answer = }")
