b22db00baa208cccdf1c5f946efdf29783220581
Ned Batchelder <ned@nedbatchelder.com>
2023-12-11 07:23:53 -0500

    day 11, part 2

--- day11.py
+++ day11.py
@@ -4,6 +4,8 @@ import itertools
 
 from helpers import *
 
+import pytest
+
 TEST_INPUT = string_lines(
     """\
 ...#......
@@ -53,22 +55,22 @@ def gaps(galaxies):
 def test_gaps():
     assert gaps(parse_galaxies(TEST_INPUT)) == ({2, 5, 8}, {3, 7})
 
-def distance(g1, g2, xgaps, ygaps):
+def distance(g1, g2, xgaps, ygaps, expansion):
     g1x, g1y = g1
     g2x, g2y = g2
     g1x, g2x = sorted([g1x, g2x])
     g1y, g2y = sorted([g1y, g2y])
     extrax = sum(1 for x in xgaps if g1x < x < g2x)
     extray = sum(1 for y in ygaps if g1y < y < g2y)
-    return (g2x - g1x) + extrax + (g2y - g1y) + extray
+    return (g2x - g1x) + extrax * (expansion - 1) + (g2y - g1y) + extray * (expansion - 1)
 
 
-def part1(lines):
+def part1(lines, expansion=2):
     galaxies = parse_galaxies(lines)
     xgaps, ygaps = gaps(galaxies)
     total = 0
     for g1, g2 in itertools.combinations(galaxies, r=2):
-        total += distance(g1, g2, xgaps, ygaps)
+        total += distance(g1, g2, xgaps, ygaps, expansion=expansion)
     return total
 
 
@@ -81,15 +83,11 @@ if __name__ == "__main__":
     print(f"Part 1: {answer = }")
 
 
-# def part2(lines):
-#     ...
-#
-#
-# def test_part2():
-#     assert part2(TEST_INPUT) == 123456
-#
-#
-#
-# if __name__ == "__main__":
-#     answer = part2(file_lines("day11_input.txt"))
-#     print(f"Part 2: {answer = }")
+@pytest.mark.parametrize("expansion, answer", [(10, 1030), (100, 8410)])
+def test_part2(expansion, answer):
+    assert part1(TEST_INPUT, expansion=expansion) == answer
+
+
+if __name__ == "__main__":
+    answer = part1(file_lines("day11_input.txt"), expansion=1_000_000)
+    print(f"Part 2: {answer = }")
